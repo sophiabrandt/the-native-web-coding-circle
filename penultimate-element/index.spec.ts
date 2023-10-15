77a5dbf81f1ffa3bd1b7a0627170ee226fc22c06
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-10-15 09:22:15 +0200

    feat: create singly linked list which one can append to & traverse

--- penultimate-element/index.spec.ts
+++ penultimate-element/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "bun:test";
-import { Node } from ".";
+import { List, Node } from ".";
 
 describe("Index", () => {
   describe("Node", () => {
@@ -9,6 +9,71 @@ describe("Index", () => {
       const actual = node.value;
 
       expect(actual).toBe(expected);
+    });
+  });
+
+  describe("List", () => {
+    it("should create a singly linked list without an initial value", () => {
+      const sut = new List();
+      const expectedHeadValue = null;
+      const actualHeadValue = sut.head;
+      const actualTailValue = sut.tail;
+      const expectedSize = 0;
+      const actualSize = sut.size;
+
+      expect(actualHeadValue).toBe(expectedHeadValue);
+      expect(actualTailValue).toBe(expectedHeadValue);
+      expect(actualSize).toBe(expectedSize);
+    });
+
+    it("should create a singly linked list with an inital value", () => {
+      const expectedHeadValue = 99;
+      const sut = new List(expectedHeadValue);
+      const actualHeadValue = sut.head?.value;
+      const actualTailValue = sut.tail?.value;
+      const expectedSize = 1;
+      const actualSize = sut.size;
+
+      expect(actualHeadValue).toBe(expectedHeadValue);
+      expect(actualTailValue).toBe(expectedHeadValue);
+      expect(actualSize).toBe(expectedSize);
+    });
+
+    it("should append a new value to an empty list", () => {
+      const sut = new List();
+      const expectedValue = 2;
+      sut.append(expectedValue);
+      const actualValue = sut.head?.value;
+      const expectedSize = 1;
+      const actualSize = sut.size;
+
+      expect(actualValue).toBe(actualValue);
+      expect(actualSize).toBe(expectedSize);
+    });
+
+    it("should append a new value to a list with previous values", () => {
+      const sut = new List(4);
+      const expectedValue = 2;
+      sut.append(8);
+      sut.append(expectedValue);
+      const actualValue = sut.head?.next?.next?.value;
+      const expectedSize = 3;
+      const actualSize = sut.size;
+
+      expect(actualValue).toBe(actualValue);
+      expect(actualSize).toBe(expectedSize);
+    });
+
+    it("should traverse a list", () => {
+      const actual: Array<number> = [];
+      const expected = [3, 99, 1];
+
+      const sut = new List(3);
+      sut.append(99);
+      sut.append(1);
+      sut.traverse((val) => actual.push(val));
+
+      expect(actual).toEqual(expected);
     });
   });
 });
--- penultimate-element/index.ts
+++ penultimate-element/index.ts
@@ -3,9 +3,51 @@ export interface INode<T> {
   next: INode<T> | null;
 }
 
+export interface SinglyLinkedList<T> {
+  head: INode<T> | null;
+  tail: INode<T> | null;
+  append(value: T): void;
+  traverse(callback: (value: T) => void): void;
+  size: number;
+}
+
 export class Node<T> implements INode<T> {
   readonly next: INode<T> | null;
   constructor(readonly value: T) {
     this.next = null;
+  }
+}
+
+export class List<T> implements SinglyLinkedList<T> {
+  head: INode<T> | null = null;
+  tail: INode<T> | null = null;
+  size: number = 0;
+
+  constructor(initialValue?: T) {
+    if (initialValue !== undefined) {
+      const newNode = new Node(initialValue);
+      this.head = newNode;
+      this.tail = newNode;
+      this.size = 1;
+    }
+  }
+
+  append(value: T): void {
+    const newNode = new Node(value);
+    if (this.tail) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    this.tail = newNode;
+    this.size++;
+  }
+
+  traverse(callback: (value: T) => void): void {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      callback(currentNode.value);
+      currentNode = currentNode.next;
+    }
   }
 }
