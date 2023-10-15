e44751e3f470ae828bcadbc7a90d59bddaaac91d
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-10-15 12:36:37 +0200

    feat: remove nth element from list

--- penultimate-element/index.spec.ts
+++ penultimate-element/index.spec.ts
@@ -13,67 +13,158 @@ describe("Index", () => {
   });
 
   describe("List", () => {
-    it("should create a singly linked list without an initial value", () => {
-      const sut = new List();
-      const expectedHeadValue = null;
-      const actualHeadValue = sut.head;
-      const actualTailValue = sut.tail;
-      const expectedSize = 0;
-      const actualSize = sut.size;
+    describe("should create", () => {
+      it("a singly linked list without an initial value", () => {
+        const sut = new List();
+        const expectedHeadValue = null;
+        const actualHeadValue = sut.head;
+        const actualTailValue = sut.tail;
+        const expectedSize = 0;
+        const actualSize = sut.size;
 
-      expect(actualHeadValue).toBe(expectedHeadValue);
-      expect(actualTailValue).toBe(expectedHeadValue);
-      expect(actualSize).toBe(expectedSize);
+        expect(actualHeadValue).toBe(expectedHeadValue);
+        expect(actualTailValue).toBe(expectedHeadValue);
+        expect(actualSize).toBe(expectedSize);
+      });
+
+      it("a singly linked list with an inital value", () => {
+        const expectedHeadValue = 99;
+        const sut = new List(expectedHeadValue);
+        const actualHeadValue = sut.head?.value;
+        const actualTailValue = sut.tail?.value;
+        const expectedSize = 1;
+        const actualSize = sut.size;
+
+        expect(actualHeadValue).toBe(expectedHeadValue);
+        expect(actualTailValue).toBe(expectedHeadValue);
+        expect(actualSize).toBe(expectedSize);
+      });
     });
 
-    it("should create a singly linked list with an inital value", () => {
-      const expectedHeadValue = 99;
-      const sut = new List(expectedHeadValue);
-      const actualHeadValue = sut.head?.value;
-      const actualTailValue = sut.tail?.value;
-      const expectedSize = 1;
-      const actualSize = sut.size;
+    describe("should append", () => {
+      it("a new value to an empty list", () => {
+        const sut = new List();
+        const expectedValue = 2;
+        sut.append(expectedValue);
+        const actualValue = sut.head?.value;
+        const expectedSize = 1;
+        const actualSize = sut.size;
 
-      expect(actualHeadValue).toBe(expectedHeadValue);
-      expect(actualTailValue).toBe(expectedHeadValue);
-      expect(actualSize).toBe(expectedSize);
+        expect(actualValue).toBe(actualValue);
+        expect(actualSize).toBe(expectedSize);
+      });
+
+      it("a new value to a list with previous values", () => {
+        const sut = new List(4);
+        const expectedValue = 2;
+        sut.append(8);
+        sut.append(expectedValue);
+        const actualValue = sut.head?.next?.next?.value;
+        const expectedSize = 3;
+        const actualSize = sut.size;
+
+        expect(actualValue).toBe(actualValue);
+        expect(actualSize).toBe(expectedSize);
+      });
     });
 
-    it("should append a new value to an empty list", () => {
-      const sut = new List();
-      const expectedValue = 2;
-      sut.append(expectedValue);
-      const actualValue = sut.head?.value;
-      const expectedSize = 1;
-      const actualSize = sut.size;
+    describe("should traverse", () => {
+      it("any list", () => {
+        const actual: Array<number> = [];
+        const expected = [3, 99, 1];
 
-      expect(actualValue).toBe(actualValue);
-      expect(actualSize).toBe(expectedSize);
+        const sut = new List(3);
+        sut.append(99);
+        sut.append(1);
+        sut.traverse((val) => actual.push(val));
+
+        expect(actual).toEqual(expected);
+      });
     });
 
-    it("should append a new value to a list with previous values", () => {
-      const sut = new List(4);
-      const expectedValue = 2;
-      sut.append(8);
-      sut.append(expectedValue);
-      const actualValue = sut.head?.next?.next?.value;
-      const expectedSize = 3;
-      const actualSize = sut.size;
+    describe("should remove nth element from end of list", () => {
+      it("for element greater than the list size", () => {
+        const actual: Array<number> = [];
+        const expected = [8, 1];
 
-      expect(actualValue).toBe(actualValue);
-      expect(actualSize).toBe(expectedSize);
-    });
+        const sut = new List(8);
+        sut.append(1);
 
-    it("should traverse a list", () => {
-      const actual: Array<number> = [];
-      const expected = [3, 99, 1];
+        sut.removeNthElementFromEndOfList(999);
+        sut.traverse((val) => actual.push(val));
 
-      const sut = new List(3);
-      sut.append(99);
-      sut.append(1);
-      sut.traverse((val) => actual.push(val));
+        expect(actual).toEqual(expected);
+      });
 
-      expect(actual).toEqual(expected);
+      it("for 0th element", () => {
+        const actual: Array<number> = [];
+        const expected = [8, 1];
+
+        const sut = new List(8);
+        sut.append(1);
+
+        sut.removeNthElementFromEndOfList(0);
+        sut.traverse((val) => actual.push(val));
+
+        expect(actual).toEqual(expected);
+      });
+
+      it("for first element from a list", () => {
+        const actual: Array<number> = [];
+        const expected = [1, 20, 0, 12];
+
+        const sut = new List(8);
+        sut.append(1);
+        sut.append(20);
+        sut.append(0);
+        sut.append(12);
+
+        sut.removeNthElementFromEndOfList(5);
+        sut.traverse((val) => actual.push(val));
+
+        expect(actual).toEqual(expected);
+      });
+
+      it("for first element tail should be null for an empty list", () => {
+        const sut = new List(8);
+        sut.removeNthElementFromEndOfList(1);
+        const actual = sut.tail;
+        const expected = null;
+
+        expect(actual).toEqual(expected);
+      });
+
+      it("for last element from a list", () => {
+        const actual: Array<number> = [];
+        const expected = [8, 1, 20, 0];
+
+        const sut = new List(8);
+        sut.append(1);
+        sut.append(20);
+        sut.append(0);
+        sut.append(12);
+
+        sut.removeNthElementFromEndOfList(1);
+        sut.traverse((val) => actual.push(val));
+
+        expect(actual).toEqual(expected);
+      });
+
+      it("for nth element from a list", () => {
+        const actual: Array<number> = [];
+        const expected = [8, 1, 20, 12];
+
+        const sut = new List(8);
+        sut.append(1);
+        sut.append(20);
+        sut.append(0);
+        sut.append(12);
+
+        sut.removeNthElementFromEndOfList(2);
+        sut.traverse((val) => actual.push(val));
+
+        expect(actual).toEqual(expected);
+      });
     });
   });
 });
--- penultimate-element/index.ts
+++ penultimate-element/index.ts
@@ -8,6 +8,7 @@ export interface SinglyLinkedList<T> {
   tail: INode<T> | null;
   append(value: T): void;
   traverse(callback: (value: T) => void): void;
+  removeNthElementFromEndOfList(n: number): void;
   size: number;
 }
 
@@ -49,5 +50,44 @@ export class List<T> implements SinglyLinkedList<T> {
       callback(currentNode.value);
       currentNode = currentNode.next;
     }
+  }
+
+  removeNthElementFromEndOfList(n: number): void {
+    // if n is greater than the length of the list,
+    // or n is 0,
+    // do nothing
+    if (n > this.size || n === 0) {
+      return;
+    }
+
+    if (n === this.size) {
+      // if n is equal to the size of the list, remove the first element
+      this.head = this.head?.next ?? null;
+      if (!this.head) {
+        // list is empty, so set the tail to null, too
+        this.tail = null;
+      }
+    } else {
+      let lead = this.head;
+      let follow = this.head;
+
+      // move the lead pointer
+      for (let i = 0; i < n; i++) {
+        if (lead !== null) {
+          lead = lead.next;
+        }
+      }
+
+      // move both pointers until lead reaches the end
+      while (lead && lead.next !== null) {
+        lead = lead.next;
+        follow = follow?.next ?? null;
+      }
+
+      if (follow && follow.next) {
+        follow.next = follow.next.next;
+      }
+    }
+    this.size--;
   }
 }
